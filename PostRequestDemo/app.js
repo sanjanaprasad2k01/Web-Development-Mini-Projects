a3b824e023a18f8ba5f94f10ac0817c7a628f965
unknown <sahil.jamwal78625@gmail.com>
2019-10-19 08:34:30 +0530

    commit

--- PostRequestDemo/app.js
+++ PostRequestDemo/app.js
@@ -1,12 +1,30 @@
 var express=require('express');
 var app=express();
+var bodyparser=require('body-parser');
 
 
 app.set("view engine","ejs");
+app.use(bodyparser.urlencoded({extended:true}));
+
+var friendList=["Anmol","Akshat","Prachi","Apoorva","Saksham"];
 
 
+//routes
 app.get("/",function(req,res){
 	res.render("home");
+});
+
+
+
+app.post("/addfriend",function(req,res){
+	var newfriend=req.body.newfriend;
+	friendList.push(newfriend);
+	res.redirect("/friends");
+});
+
+
+app.get("/friends",function(req,res){
+	res.render("friends",{friends:friendList});
 });
 
 
@@ -14,9 +32,7 @@ app.get("/",function(req,res){
 
 
 
-
-
-
+//listeners
 app.listen(3000,function(){
 	console.log("Server Started Successfully!!");
 });
